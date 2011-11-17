479f646e63023f96270606bbbb8fd4b538231538
Oscar Persson <dallas.persson@gmail.com>
2011-11-17 11:20:06 +0100

    Added some comments

--- TS_CompositeSchedule.php
+++ TS_CompositeSchedule.php
@@ -10,10 +10,13 @@ class TS_CompositeSchedule extends TS_Schedule
 		$this->schedules = $schedules;
 	}
 	
+	// Checks if a specific timestamp is availible 
+	// in the composite schedual, and returns true or false.
 	public function getAvailability($timestamp)
 	{
 		$available = false;
 		
+		// Loop through all of the objects scheduals
 		foreach($this->schedules as $schedule)
 			if($schedule->getAvailability($timestamp))
 			{
